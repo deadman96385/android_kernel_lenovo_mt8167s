c10f82229d07b2e8b14c77244b84e73ed86d18e5
Hidalgo Huang <hidalgo.huang@mediatek.com>
2016-06-13 14:36:26 +0800

    [ALPS02611174] ASoC: mt8167: Add initial implementation
    Add machine driver, cpu dai driver and platform driver with dpcm support.
    
    Change-Id: I335454227db54297269def4645c2e53bcb639a29
    Signed-off-by: Hidalgo Huang <hidalgo.huang@mediatek.com>
    CR-Id: ALPS02611174
    Feature: Audio Playback(*)

---
+++ sound/soc/mediatek/mt_soc_audio_8167/mt8167_evb.c
@@ -0,0 +1,254 @@
+/*
+ * mt8167_evb.c  --  MT8167 EVB ALSA SoC machine driver
+ *
+ * Copyright (c) 2016 MediaTek Inc.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 and
+ * only version 2 as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ */
+
+#include <linux/module.h>
+#include <sound/soc.h>
+#include <linux/gpio.h>
+
+
+/* Digital audio interface glue - connects codec <---> CPU */
+static struct snd_soc_dai_link mt8167_evb_dais[] = {
+	/* Front End DAI links */
+	{
+		.name = "DL1 Playback",
+		.stream_name = "MultiMedia1_PLayback",
+		.cpu_dai_name = "DL1",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.trigger = {
+			SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST
+		},
+		.dynamic = 1,
+		.dpcm_playback = 1,
+	},
+	{
+		.name = "VUL Capture",
+		.stream_name = "MultiMedia1_Capture",
+		.cpu_dai_name = "VUL",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.trigger = {
+			SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST
+		},
+		.dynamic = 1,
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "HDMI",
+		.stream_name = "HMDI_PLayback",
+		.cpu_dai_name = "HDMI",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.trigger = {
+			SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST
+		},
+		.dynamic = 1,
+		.dpcm_playback = 1,
+	},
+	{
+		.name = "AWB Capture",
+		.stream_name = "DL1_AWB_Record",
+		.cpu_dai_name = "AWB",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.trigger = {
+			SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST
+		},
+		.dynamic = 1,
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "DL2 Playback",
+		.stream_name = "MultiMedia2_PLayback",
+		.cpu_dai_name = "DL2",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.trigger = {
+			SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST
+		},
+		.dynamic = 1,
+		.dpcm_playback = 1,
+	},
+	{
+		.name = "DAI Capture",
+		.stream_name = "VOIP_Call_BT_Capture",
+		.cpu_dai_name = "DAI",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.trigger = {
+			SND_SOC_DPCM_TRIGGER_POST,
+			SND_SOC_DPCM_TRIGGER_POST
+		},
+		.dynamic = 1,
+		.dpcm_capture = 1,
+	},
+#ifdef CONFIG_MTK_BTCVSD_ALSA
+	{
+		.name = "BTCVSD_RX",
+		.stream_name = "BTCVSD_Capture",
+		.cpu_dai_name = "snd-soc-dummy-dai",
+		.platform_name = "mt-soc-btcvsd-rx-pcm",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+	},
+	{
+		.name = "BTCVSD_TX",
+		.stream_name = "BTCVSD_Playback",
+		.cpu_dai_name = "snd-soc-dummy-dai",
+		.platform_name = "mt-soc-btcvsd-tx-pcm",
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+	},
+#endif
+	/* Back End DAI links */
+	{
+		.name = "EXT Codec",
+		.cpu_dai_name = "I2S",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+			   SND_SOC_DAIFMT_CBS_CFS,
+		.dpcm_playback = 1,
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "2ND EXT Codec",
+		.cpu_dai_name = "2ND I2S",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dai_fmt = SND_SOC_DAIFMT_I2S | SND_SOC_DAIFMT_NB_NF |
+			   SND_SOC_DAIFMT_CBS_CFS,
+		.dpcm_playback = 1,
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "MTK Codec Out",
+		.cpu_dai_name = "INT ADDA",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dpcm_playback = 1,
+	},
+	{
+		.name = "MTK Codec In",
+		.cpu_dai_name = "INT ADDA",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "HDMI BE",
+		.cpu_dai_name = "HDMIO",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dpcm_playback = 1,
+	},
+	{
+		.name = "DL BE",
+		.cpu_dai_name = "DL Input",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "MRG BT BE",
+		.cpu_dai_name = "MRG BT",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dpcm_playback = 1,
+		.dpcm_capture = 1,
+	},
+	{
+		.name = "PCM0 BE",
+		.cpu_dai_name = "PCM0",
+		.no_pcm = 1,
+		.codec_name = "snd-soc-dummy",
+		.codec_dai_name = "snd-soc-dummy-dai",
+		.dpcm_playback = 1,
+		.dpcm_capture = 1,
+	},
+};
+
+static struct snd_soc_card mt8167_evb_card = {
+	.name = "mt-snd-card",
+	.owner = THIS_MODULE,
+	.dai_link = mt8167_evb_dais,
+	.num_links = ARRAY_SIZE(mt8167_evb_dais),
+};
+
+static int mt8167_evb_dev_probe(struct platform_device *pdev)
+{
+	struct snd_soc_card *card = &mt8167_evb_card;
+	struct device_node *platform_node;
+	int ret, i;
+
+	platform_node = of_parse_phandle(pdev->dev.of_node,
+					 "mediatek,platform", 0);
+	if (!platform_node) {
+		dev_err(&pdev->dev, "Property 'platform' missing or invalid\n");
+		return -EINVAL;
+	}
+
+	for (i = 0; i < card->num_links; i++) {
+		if (mt8167_evb_dais[i].platform_name)
+			continue;
+		mt8167_evb_dais[i].platform_of_node = platform_node;
+	}
+
+	card->dev = &pdev->dev;
+
+	ret = devm_snd_soc_register_card(&pdev->dev, card);
+	if (ret)
+		dev_err(&pdev->dev, "%s snd_soc_register_card fail %d\n",
+			__func__, ret);
+
+	return ret;
+}
+
+static const struct of_device_id mt8167_evb_dt_match[] = {
+	{ .compatible = "mediatek,mt8167-mt6392", },
+	{ }
+};
+MODULE_DEVICE_TABLE(of, mt8167_evb_dt_match);
+
+static struct platform_driver mt8167_evb_mach_driver = {
+	.driver = {
+		   .name = "mt8167-mt6392",
+		   .of_match_table = mt8167_evb_dt_match,
+#ifdef CONFIG_PM
+		   .pm = &snd_soc_pm_ops,
+#endif
+	},
+	.probe = mt8167_evb_dev_probe,
+};
+
+module_platform_driver(mt8167_evb_mach_driver);
+
+/* Module information */
+MODULE_DESCRIPTION("MT8167 ALSA SoC machine driver");
+MODULE_LICENSE("GPL v2");
+MODULE_ALIAS("platform:mt8167-mt6392");
+
