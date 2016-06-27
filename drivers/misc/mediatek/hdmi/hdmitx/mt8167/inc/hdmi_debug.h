b493d8c5ee4912dbe4138179b5cc17d430c614ea
Junzhi Zhao <junzhi.zhao@mediatek.com>
2016-06-27 16:40:02 +0800

    [ALPS02778881] HDMI: Add 8167 hdmi driver
    [Detail]
    Kernel-4.4 does not include the 8167 hdmi driver
    [Solution]
    Add the 8167 hdmi driver
    
    Change-Id: I09930805b2a6a51ac33fb79b8a621a049c3c976e
    CR-Id: ALPS02778881
    Feature: HDMI
    Signed-off-by: Junzhi Zhao <junzhi.zhao@mediatek.com>

---
+++ drivers/misc/mediatek/hdmi/hdmitx/mt8167/inc/hdmi_debug.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (C) 2015 MediaTek Inc.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ */
+
+#ifndef _MT_HDMI_DEBUG_H_
+#define _MT_HDMI_DEBUG_H_
+
+#ifdef CONFIG_MTK_INTERNAL_HDMI_SUPPORT
+#include "hdmitable.h"
+#include "hdmiedid.h"
+
+extern unsigned char _bHdcpOff;
+
+extern HDMI_AV_INFO_T _stAvdAVInfo;
+extern struct HDMI_SINK_AV_CAP_T _HdmiSinkAvCap;
+extern unsigned char cDstStr[50];
+extern const unsigned char _cFsStr[][7];
+extern unsigned char hdmi_plug_test_mode;
+/* unsigned int hdmi_plug_out_delay; */
+/* unsigned int hdmi_plug_in_delay; */
+extern unsigned char is_user_mute_hdmi;
+extern unsigned char is_user_mute_hdmi_audio;
+extern void hdmi_force_plug_out(void);
+extern void hdmi_force_plug_in(void);
+extern unsigned char hdmi_hdcp_for_avmute;
+extern void mt_hdmi_show_info(char *pbuf);
+extern void mt_hdmi_debug_write(char *pbuf);
+
+#endif
+#endif
