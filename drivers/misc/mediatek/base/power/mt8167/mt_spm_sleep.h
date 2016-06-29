5313d18c8939432528231bcd08510fa68a7f39f9
YT Lee <yt.lee@mediatek.com>
2016-06-29 17:01:03 +0800

    [ALPS02781789] spm: remove unused files
    [Detail]
    Remove unused files
    
    Change-Id: I1e8f36f78fdc8460dda6eda5f9109e84abbe5a35
    Signed-off-by: YT Lee <yt.lee@mediatek.com>
    Feature:Low power
    CR-Id:ALPS02781789

--- drivers/misc/mediatek/base/power/mt8167/mt_spm_sleep.h
+++ drivers/misc/mediatek/base/power/mt8167/mt_spm_sleep.h
@@ -23,9 +23,6 @@ extern int spm_set_sleep_wakesrc(u32 wakesrc, bool enable, bool replace);
 extern u32 spm_get_sleep_wakesrc(void);
 extern wake_reason_t spm_go_to_sleep(u32 spm_flags, u32 spm_data);
 
-extern bool spm_is_md_sleep(void);
-extern bool spm_is_md1_sleep(void);
-extern bool spm_is_md2_sleep(void);
 extern bool spm_is_conn_sleep(void);
 extern void spm_set_wakeup_src_check(void);
 extern bool spm_check_wakeup_src(void);
