57075d3e022b25be821300afba0808f9047aa27a
Junzhou Chen <Junzhou.Chen@mediatek.com>
2016-09-30 17:09:51 +0800

    [ALPS02930659] DISP: mt8167 enable config.
    [Detail]
    enable MUTI_PASS/TIME_SHARING/CAP_SWITCHABLE
    
    [Solution]
    
    Change-Id: Ib897da450604db0f1403f9f35d2c47c1a68f2485
    Signed-off-by: Junzhou Chen <Junzhou.Chen@mediatek.com>
    CR-Id: ALPS02930659
    Feature: Driver

--- drivers/misc/mediatek/video/mt8167/videox/disp_drv_platform.h
+++ drivers/misc/mediatek/video/mt8167/videox/disp_drv_platform.h
@@ -77,13 +77,13 @@
 /**
  * Enable Multipass support.
  */
-/* #define OVL_MULTIPASS_SUPPORT */
+#define OVL_MULTIPASS_SUPPORT
 
 /**
  * Enable Ovl time-sharing.
  * turned off for FPGA and bringup
  */
-/* #define OVL_TIME_SHARING */
+#define OVL_TIME_SHARING
 
 /**
  * Build CMDQ command in trigger stage.
@@ -218,7 +218,7 @@ enum DISP_SESSION_ENUM {
 
 	#define DISP_INTERNAL_BUFFER_COUNT 1
 #else
-	#define DISP_HW_MODE_CAP DISP_OUTPUT_CAP_DIRECT_LINK
+	#define DISP_HW_MODE_CAP DISP_OUTPUT_CAP_SWITCHABLE
 	#ifdef OVL_MULTIPASS_SUPPORT
 		#define DISP_HW_PASS_MODE DISP_OUTPUT_CAP_MULTI_PASS
 	#else
