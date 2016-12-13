75f32333784b6c0a4bb1d1b3a852f1965fd2dc73
Tianping Fang <Tianping.Fang@mediatek.com>
2016-12-13 19:05:33 +0800

    [ALPS03065261] USB: add protection for usb clock dynamic control
    [Detail]
    need remove spin lock for clk_prepare/clk_unprepare context
    for port0 clock dynamic control.
    
    Change-Id: I1a51f3757c5e019f3493a6f5c395e127e667d24a
    Signed-off-by: Tianping Fang <Tianping.Fang@mediatek.com>
    CR-Id: ALPS03065261
    Feature: USB

--- drivers/misc/mediatek/usb20/mt8167/usb20.c
+++ drivers/misc/mediatek/usb20/mt8167/usb20.c
@@ -414,6 +414,9 @@ void do_connection_work(struct work_struct *data)
 		} else {
 			DBG(0, "already lock\n");
 		}
+		#ifdef CONFIG_MTK_MUSB_PORT0_LOWPOWER_MODE
+		spin_unlock_irqrestore(&mtk_musb->lock, flags);
+		#endif
 		/* note this already put SOFTCON */
 		musb_start(mtk_musb);
 
@@ -421,17 +424,28 @@ void do_connection_work(struct work_struct *data)
 	else if (mtk_musb->power && (usb_in == false)) {
 		/* disable usb */
 		musb_stop(mtk_musb);
+		#ifdef CONFIG_MTK_MUSB_PORT0_LOWPOWER_MODE
+		spin_lock_irqsave(&mtk_musb->lock, flags);
+		#endif
 		if (wake_lock_active(&mtk_musb->usb_lock)) {
 			DBG(0, "unlock\n");
 			wake_unlock(&mtk_musb->usb_lock);
 		} else {
 			DBG(0, "lock not active\n");
 		}
-
-	} else
+		#ifdef CONFIG_MTK_MUSB_PORT0_LOWPOWER_MODE
+		spin_unlock_irqrestore(&mtk_musb->lock, flags);
+		#endif
+	} else {
 		DBG(0, "do nothing, usb_in:%d, power:%d\n",
 				usb_in, mtk_musb->power);
+		#ifdef CONFIG_MTK_MUSB_PORT0_LOWPOWER_MODE
+		spin_unlock_irqrestore(&mtk_musb->lock, flags);
+		#endif
+	}
+	#ifndef CONFIG_MTK_MUSB_PORT0_LOWPOWER_MODE
 	spin_unlock_irqrestore(&mtk_musb->lock, flags);
+	#endif
 
 }
 
