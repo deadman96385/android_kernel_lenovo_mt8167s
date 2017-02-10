002ecff9461852dda032c47a7f41cafdd007032c
ALPS.Release <ALPS.Release@mediatek.com>
2017-02-11 03:17:38 +0800

    [ALPS03072352] GPU: Fix KE in GED DVFS.
    [Detail]
    Using mutex in ISR.
    IMGs loading API need mutex lock while retrieving loading
    which is not acceptable in current design.
    
    Change-Id: I4b86744ed61f046e7fc642d55541504d68d28661
    Feature: GPU
    CR-Id: ALPS03072352
    MTK-Commit-Id: 0abe0a3ee683f3af84e10a58bf5a08430eb55d41

--- drivers/misc/mediatek/gpu/ged/src/ged_monitor_3D_fence.c
+++ drivers/misc/mediatek/gpu/ged/src/ged_monitor_3D_fence.c
@@ -72,7 +72,7 @@ static void ged_sync_cb(struct sync_fence *fence, struct sync_fence_waiter *wait
 
 	ged_monitor_3D_fence_notify();
 #ifdef GED_DVFS_ENABLE
-#if defined(CONFIG_MACH_MT6799)
+#if 1
 	/* FIX-ME: IMG's loading API requires mutext lock which is not suitable here */;
 #else
 	ged_dvfs_cal_gpu_utilization_force();
@@ -92,6 +92,10 @@ static void ged_monitor_3D_fence_work_cb(struct work_struct *psWork)
 
 #ifdef GED_DEBUG_MONITOR_3D_FENCE
 	ged_log_buf_print(ghLogBuf_GED, "ged_monitor_3D_fence_work_cb");
+#endif
+
+#ifdef GED_DVFS_ENABLE
+	ged_dvfs_cal_gpu_utilization_force();
 #endif
 
 	if (atomic_sub_return(1, &g_i32Count) < 1)
