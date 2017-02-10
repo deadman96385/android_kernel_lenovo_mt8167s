c7ee3f409e660c3f39e121a7e5118c365a1f669c
ALPS.Release <ALPS.Release@mediatek.com>
2017-02-10 23:23:45 +0800

    [ALPS02945992] camera: update test pattern crc value
    [Detail] as title, set the right crc value for RFC&FFC.
    
    Change-Id: Ib8f94ab6df442366d7a0e1c271997ddc1d6d68bd
    CR-Id: ALPS02945992
    Feature: Camera
    MTK-Commit-Id: cb9c2e479656335844f9e4dfc29c2ee266855f8b

--- drivers/misc/mediatek/imgsensor/src/mt8167/gc2355_mipi_raw/gc2355mipi_Sensor.c
+++ drivers/misc/mediatek/imgsensor/src/mt8167/gc2355_mipi_raw/gc2355mipi_Sensor.c
@@ -63,7 +63,7 @@ static imgsensor_info_struct imgsensor_info = {
 	.sensor_id = GC2355_SENSOR_ID,	/* record sensor id defined in Kd_imgsensor.h */
 
 	/* .checksum_value = 0x9d1c9dad,        //checksum value for Camera Auto Test */
-	.checksum_value = 0xffd8e112,	/* checksum value for Camera Auto Test */
+	.checksum_value = 0x31fb1318,	/* checksum value for Camera Auto Test */
 
 	.pre = {
 		.pclk = 42000000,	/* record different mode's pclk */
