17773ef24a6b7eb8b6a1cffacb466d0e2f738231
ALPS.Release <ALPS.Release@mediatek.com>
2017-02-10 20:42:04 +0800

    [ALPS02613621] pmic: mt8167: Add support for mt6392 pmic
    Add legacy pmic APIs for legacy turnkey driver.
    1. add pmic misc driver "pmic_mt6392.c".
    This file provides pmic_read_interface/pmic_config_interface
    and pmic initial setting from DE.
    2. provide upmu_* interfaces
    
    Change-Id: I4b10fe36aba2bae721e61bdb29e5991991da3330
    CR-Id: ALPS02613621
    Feature: PMIC
    MTK-Commit-Id: bbcefd654179ec1a699eacd628abf4a9c9ddec17

---
+++ drivers/misc/mediatek/power/mt8167/pmic_mt6392.c
@@ -0,0 +1,185 @@
+/*
+ * Copyright (c) 2016 MediaTek Inc.
+ * Author: Chen Zhong <chen.zhong@mediatek.com>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */
+
+#include <linux/delay.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/slab.h>
+#include <linux/kernel.h>
+#include <linux/regmap.h>
+#include <linux/spinlock.h>
+#include <linux/mutex.h>
+#include <linux/interrupt.h>
+#include <linux/platform_device.h>
+#include <linux/of.h>
+#include <linux/of_address.h>
+#include <linux/kernel.h>
+#include <linux/version.h>
+#include <linux/io.h>
+#include <linux/mfd/mt6397/core.h>
+#include <linux/mfd/mt6392/registers.h>
+
+#include <mt-plat/upmu_common.h>
+
+struct regmap *pwrap_regmap;
+
+unsigned int pmic_read_interface(unsigned int RegNum, unsigned int *val, unsigned int MASK, unsigned int SHIFT)
+{
+	unsigned int return_value = 0;
+	unsigned int pmic_reg = 0;
+
+	return_value = regmap_read(pwrap_regmap, RegNum, &pmic_reg);
+	if (return_value != 0) {
+		pr_err("[Power/PMIC][pmic_read_interface] Reg[%x]= pmic_wrap read data fail\n", RegNum);
+		return return_value;
+	}
+
+	pmic_reg &= (MASK << SHIFT);
+	*val = (pmic_reg >> SHIFT);
+
+	return return_value;
+}
+
+unsigned int pmic_config_interface(unsigned int RegNum, unsigned int val, unsigned int MASK, unsigned int SHIFT)
+{
+	unsigned int return_value = 0;
+	unsigned int pmic_reg = 0;
+
+	return_value = regmap_read(pwrap_regmap, RegNum, &pmic_reg);
+	if (return_value != 0) {
+		pr_err("[Power/PMIC][pmic_config_interface] Reg[%x]= pmic_wrap read data fail\n", RegNum);
+		return return_value;
+	}
+
+	pmic_reg &= ~(MASK << SHIFT);
+	pmic_reg |= (val << SHIFT);
+
+	return_value = regmap_write(pwrap_regmap, RegNum, pmic_reg);
+	if (return_value != 0) {
+		pr_err("[Power/PMIC][pmic_config_interface] Reg[%x]= pmic_wrap read data fail\n", RegNum);
+		return return_value;
+	}
+
+	return return_value;
+}
+
+u32 upmu_get_reg_value(u32 reg)
+{
+	u32 reg_val = 0;
+
+	pmic_read_interface(reg, &reg_val, 0xFFFF, 0x0);
+
+	return reg_val;
+}
+EXPORT_SYMBOL(upmu_get_reg_value);
+
+void upmu_set_reg_value(u32 reg, u32 reg_val)
+{
+	pmic_config_interface(reg, reg_val, 0xFFFF, 0x0);
+}
+
+u32 g_reg_value;
+static ssize_t show_pmic_access(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	pr_notice("[show_pmic_access] 0x%x\n", g_reg_value);
+	return sprintf(buf, "%u\n", g_reg_value);
+}
+
+static ssize_t store_pmic_access(struct device *dev, struct device_attribute *attr, const char *buf,
+				 size_t size)
+{
+	int ret = 0;
+	char temp_buf[32];
+	char *pvalue;
+	unsigned int reg_value = 0;
+	unsigned int reg_address = 0;
+
+	strncpy(temp_buf, buf, sizeof(temp_buf));
+	temp_buf[sizeof(temp_buf) - 1] = 0;
+	pvalue = temp_buf;
+
+	if (size != 0) {
+		if (size > 5) {
+			ret = kstrtouint(strsep(&pvalue, " "), 16, &reg_address);
+			if (ret)
+				return ret;
+			ret = kstrtouint(pvalue, 16, &reg_value);
+			if (ret)
+				return ret;
+			pr_notice("[store_pmic_access] write PMU reg 0x%x with value 0x%x !\n",
+				  reg_address, reg_value);
+			ret = pmic_config_interface(reg_address, reg_value, 0xFFFF, 0x0);
+		} else {
+			ret = kstrtouint(pvalue, 16, &reg_address);
+			if (ret)
+				return ret;
+			ret = pmic_read_interface(reg_address, &g_reg_value, 0xFFFF, 0x0);
+			pr_notice("[store_pmic_access] read PMU reg 0x%x with value 0x%x !\n",
+				  reg_address, g_reg_value);
+			pr_notice
+			    ("[store_pmic_access] Please use \"cat pmic_access\" to get value\r\n");
+		}
+	}
+	return size;
+}
+
+static DEVICE_ATTR(pmic_access, 0664, show_pmic_access, store_pmic_access);	/* 664 */
+
+void PMIC_INIT_SETTING_V1(void)
+{
+	/* to be do */
+	/* put init setting from DE/SA */
+}
+
+static int mt6392_pmic_probe(struct platform_device *dev)
+{
+	int ret_val = 0;
+	struct mt6397_chip *mt6392_chip = dev_get_drvdata(dev->dev.parent);
+
+	pr_debug("[Power/PMIC] ******** MT6392 pmic driver probe!! ********\n");
+
+	pwrap_regmap = mt6392_chip->regmap;
+
+	/* get PMIC CID */
+	ret_val = upmu_get_cid();
+	pr_notice("Power/PMIC MT6392 PMIC CID=0x%x\n", ret_val);
+
+	/* pmic initial setting */
+	PMIC_INIT_SETTING_V1();
+
+	device_create_file(&(dev->dev), &dev_attr_pmic_access);
+	return 0;
+}
+
+static const struct platform_device_id mt6392_pmic_ids[] = {
+	{"mt6392-pmic", 0},
+	{ /* sentinel */ },
+};
+MODULE_DEVICE_TABLE(platform, mt6392_pmic_ids);
+
+static const struct of_device_id mt6392_pmic_of_match[] = {
+	{ .compatible = "mediatek,mt6392-pmic", },
+	{ /* sentinel */ },
+};
+MODULE_DEVICE_TABLE(of, mt6392_pmic_of_match);
+
+static struct platform_driver mt6392_pmic_driver = {
+	.driver = {
+		.name = "mt6392-pmic",
+		.of_match_table = of_match_ptr(mt6392_pmic_of_match),
+	},
+	.probe = mt6392_pmic_probe,
+	.id_table = mt6392_pmic_ids,
+};
+
+module_platform_driver(mt6392_pmic_driver);
+
+MODULE_AUTHOR("Chen Zhong <chen.zhong@mediatek.com>");
+MODULE_DESCRIPTION("PMIC Misc Setting Driver for MediaTek MT6392 PMIC");
+MODULE_LICENSE("GPL v2");
