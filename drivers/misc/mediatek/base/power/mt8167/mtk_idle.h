63b1483846be31bc8f7acf784621eea31017d8d8
ALPS.Release <ALPS.Release@mediatek.com>
2017-02-11 00:05:17 +0800

    [ALPS02818951] idle: mt8167: Add mtk_idle support for MT8167
    [Detail]
    Add initial mtk_idle support for MT8167. All idle handlers are
    disabled by default except rgidle (regular idle).
    
    Change-Id: I40baeddd7c38a48f033eef5a148b60e61a1d598b
    CR-Id: ALPS02818951
    Feature: Power Management
    MTK-Commit-Id: 87b2158e31b145d2eb0acefbc76c5ffd5801f854

---
+++ drivers/misc/mediatek/base/power/mt8167/mtk_idle.h
@@ -0,0 +1,75 @@
+#ifndef _MT_IDLE_H
+#define _MT_IDLE_H
+
+#define MT_IDLE_EN		1
+
+enum idle_lock_spm_id {
+	IDLE_SPM_LOCK_VCORE_DVFS = 0,
+};
+
+#if MT_IDLE_EN
+
+extern void idle_lock_spm(enum idle_lock_spm_id id);
+extern void idle_unlock_spm(enum idle_lock_spm_id id);
+
+extern void enable_dpidle_by_bit(int id);
+extern void disable_dpidle_by_bit(int id);
+extern void enable_soidle_by_bit(int id);
+extern void disable_soidle_by_bit(int id);
+
+extern void set_slp_spm_deepidle_flags(bool en);
+
+extern void mt_idle_init(void);
+
+#else /* !MT_IDLE_EN */
+
+static inline void idle_lock_spm(enum idle_lock_spm_id id) {}
+static inline void idle_unlock_spm(enum idle_lock_spm_id id) {}
+
+static inline void enable_dpidle_by_bit(int id) {}
+static inline void disable_dpidle_by_bit(int id) {}
+static inline void enable_soidle_by_bit(int id) {}
+static inline void disable_soidle_by_bit(int id) {}
+
+static inline void set_slp_spm_deepidle_flags(bool en) {}
+
+static inline void mt_idle_init(void) {}
+
+#endif /* MT_IDLE_EN */
+
+enum {
+	CG_CTRL0,
+	CG_CTRL1,
+	CG_CTRL2,
+	CG_MMSYS0,
+	CG_MMSYS1,
+	CG_IMGSYS,
+	CG_MFGSYS,
+	CG_AUDIO,
+	CG_VDEC0,
+	CG_VDEC1,
+
+	NR_GRPS,
+};
+
+#define CGID(grp, bit)	((grp) * 32 + bit)
+
+enum cg_clk_id {
+	NR_CLKS,
+};
+
+#ifdef _MT_IDLE_C
+
+/* TODO: remove it */
+
+extern unsigned int g_SPM_MCDI_Abnormal_WakeUp;
+
+#if defined(EN_PTP_OD) && EN_PTP_OD
+extern u32 ptp_data[3];
+#endif
+
+extern struct kobject *power_kobj;
+
+#endif /* _MT_IDLE_C */
+
+#endif
