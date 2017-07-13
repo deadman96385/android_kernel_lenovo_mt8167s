7f52ed533849764e22553f21960e2f1d9e8c8ede
Zhao Chen <zhao.chen@mediatek.com>
2017-07-13 16:10:40 +0800

    mediatek_wifi: remove read /data/misc/wifi/wifi.cfg
    mtk_wmtd: type=1400 audit(0.0:6): avc: denied { dac_override }
    Remove unused code to avoid DAC issue.
    
    Change-Id: Ie360b88408c53ce5852400ee0f1d14df6e975bb2
    Feature: mediatek_wifi

--- drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_init.c
+++ drivers/misc/mediatek/connectivity/wlan/gen3/os/linux/gl_init.c
@@ -1924,12 +1924,8 @@ static INT_32 wlanProbe(PVOID pvData)
 			u4ConfigReadLen = 0;
 			kalMemZero(pucConfigBuf, WLAN_CFG_FILE_BUF_SIZE);
 			if (pucConfigBuf) {
-				if (kalReadToFile("/storage/sdcard0/wifi.cfg", pucConfigBuf,
-						  WLAN_CFG_FILE_BUF_SIZE, &u4ConfigReadLen) == 0)
-					;
-				else
-					kalReadToFile("/data/misc/wifi/wifi.cfg", pucConfigBuf,
-							 WLAN_CFG_FILE_BUF_SIZE, &u4ConfigReadLen);
+				kalReadToFile("/storage/sdcard0/wifi.cfg", pucConfigBuf,
+						  WLAN_CFG_FILE_BUF_SIZE, &u4ConfigReadLen);
 
 				if (pucConfigBuf[0] != '\0' && u4ConfigReadLen > 0)
 					wlanCfgInit(prAdapter, pucConfigBuf, u4ConfigReadLen, 0);
