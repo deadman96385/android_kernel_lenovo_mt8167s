f8d187d7a0acbdd4649f5f92628ccb84e08bcbfb
Zhanyong Wang <zhanyong.wang@mediatek.com>
2017-10-19 15:20:48 +0800

    usb: musb: honour the return value of dma_map_single
    1. since dma_map_single() may fail it ig good to
       actually check the return code to see if it succeeded.
    
    2. do *unmap_dma_buffer* only for valid DMA addr
       musb does not use DMA buffer for ep0 but it uses
       the same giveback function *musb_g_giveback* for
       all endpoints(*musb_g_ep0_giveback* calls
       *musb_g_givebqack*). So for ep0 case request. dma
       will be '0' and will result in kernel OOPS if tried
       to *unmap_dma_buffer* for requests in ep0. Fixed it
       by doing *unmap_dma_buffer* only for valid DMA addr
       and checking that musb_ep->dma is valid when unmapping.
    
    merged commit 7b360f4287e52802b8 and 06d9db7273c7bd5b07
    
    Bug: None
    Test: ADB work on Polk
    Change-Id: I731f4740c269ac0994d66421a66456a6ed84d9d0
    Signed-off-by: Zhanyong Wang <zhanyong.wang@mediatek.com>
    CR-Id: ALPS03575857
    Feature: USB OTG

--- drivers/misc/mediatek/usb20/musb_gadget.c
+++ drivers/misc/mediatek/usb20/musb_gadget.c
@@ -137,11 +137,21 @@ static inline void map_dma_buffer(struct musb_request *request,
 #endif
 
 	if (request->request.dma == DMA_ADDR_INVALID) {
-		request->request.dma = dma_map_single(musb->controller,
-						      request->request.buf,
-						      length,
-						      request->tx
-						      ? DMA_TO_DEVICE : DMA_FROM_DEVICE);
+		dma_addr_t dma_addr;
+		int ret;
+
+		dma_addr = dma_map_single(
+				musb->controller,
+				request->request.buf,
+				length,
+				request->tx
+					? DMA_TO_DEVICE
+					: DMA_FROM_DEVICE);
+		ret = dma_mapping_error(musb->controller, dma_addr);
+		if (ret)
+			return;
+
+		request->request.dma = dma_addr;
 		request->map_state = MUSB_MAPPED;
 	} else {
 		dma_sync_single_for_device(musb->controller,
@@ -202,7 +212,10 @@ void musb_g_giveback(struct musb_ep *ep,
 
 	ep->busy = 1;
 	spin_unlock(&musb->lock);
-	unmap_dma_buffer(req, musb);
+
+	if (!dma_mapping_error(&musb->g.dev, request->dma))
+		unmap_dma_buffer(req, musb);
+
 	if (request->status == 0)
 		DBG(1, "%s done request %p,  %d/%d\n",
 		    ep->end_point.name, request, req->request.actual, req->request.length);
