899384a2d32039da46d443d4c01ef6e4c85feacb
SS Lee <ss.lee@mediatek.com>
2018-03-23 00:55:07 -0700

    [PATCH] dm-verity: add retry mechanism before mounting dm-0
    Sometimes emmc initialization took too long and caused mount failure.
    Add retry mechanism to ensure the partition nodes exist while mounting.
    
    Bug: 76018387
    Test: Make sure the retry mechanism is working from serial console log.
    Change-Id: Iaa86cbb45f803f88a59e43ad03664be5b4168f26

--- drivers/md/dm-table.c
+++ drivers/md/dm-table.c
@@ -386,13 +386,26 @@ dev_t dm_get_dev_t(const char *path)
 		}
 		kfree(dev_path);
 	} else {
-		bdev = lookup_bdev(path);
-		if (IS_ERR(bdev))
-			dev = name_to_dev_t(path);
-		else {
+		bdev = lookup_bdev(dev_path);
+		if (IS_ERR(bdev)) {
+			const unsigned int timeout_ms = DM_VERITY_WAIT_DEV_TIMEOUT_MS;
+			unsigned int wait_time_ms = 0;
+
+			while (driver_probe_done() || !(dev = name_to_dev_t(path))) {
+				DMERR("dm_get_dev_t: wait %d ms and retry...\n", DM_VERITY_RETRY_TIME_MS);
+				msleep(DM_VERITY_RETRY_TIME_MS);
+				wait_time_ms += DM_VERITY_RETRY_TIME_MS;
+				if (wait_time_ms > timeout_ms) {
+					DMERR("dm_get_dev_t: retry timeout\n");
+					DMERR("no dev found for %s\n", path);
+					break;
+				}
+			}
+		} else {
 			dev = bdev->bd_dev;
 			bdput(bdev);
 		}
+		kfree(dev_path);
 	}
 
 	return dev;
