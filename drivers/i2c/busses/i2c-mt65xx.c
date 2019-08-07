594d165a3f5926ec9dd09abe540876d5d72162c3
jg_daolongzhu <jg_daolongzhu@mediatek.com>
2019-08-07 14:28:58 +0800

    [ALPS04587575] i2c: mt8167: Add i2c ac-timimg setting interface
    [Detail] Add some I2C timing registers to meet I2C spec, we
    configures these registers via ac-timing in DTS.
    
    Bug: 138833366
    Change-Id: I4aeee0800ac0a3a9b6b9742384a60d8649e1ccab
    Signed-off-by: jg_daolongzhu <jg_daolongzhu@mediatek.com>
    CR-Id: ALPS04587575
    Feature: i2c

--- drivers/i2c/busses/i2c-mt65xx.c
+++ drivers/i2c/busses/i2c-mt65xx.c
@@ -131,6 +131,12 @@ enum I2C_REGS_OFFSET {
 	OFFSET_DEBUGCTRL = 0x68,
 	OFFSET_TRANSFER_LEN_AUX = 0x6c,
 	OFFSET_CLOCK_DIV = 0x70,
+	OFFSET_SCL_HIGH_LOW_RATIO = 0x74,
+	OFFSET_HS_SCL_HIGH_LOW_RATIO = 0x78,
+	OFFSET_SCL_MIS_COMP_POINT = 0x7c,
+	OFFSET_STA_STO_AC_TIMING = 0x80,
+	OFFSET_HS_STA_STO_AC_TIMING = 0x84,
+	OFFSET_SDA_TIMING = 0x88,
 };
 
 struct mtk_i2c_compatible {
@@ -141,6 +147,17 @@ struct mtk_i2c_compatible {
 	unsigned char aux_len_reg: 1;
 	unsigned char support_33bits: 1;
 	unsigned char timing_adjust: 1;
+};
+
+struct mtk_i2c_ac_timing {
+	u16 ext;
+	u16 inter_clk_div;
+	u16 scl_hl_ratio;
+	u16 hs_scl_hl_ratio;
+	u16 scl_mis_comp;
+	u16 sta_stop;
+	u16 hs_sta_stop;
+	u16 sda_timing;
 };
 
 struct mtk_i2c {
@@ -169,6 +186,7 @@ struct mtk_i2c {
 	u16 high_speed_reg;
 	unsigned char auto_restart;
 	bool ignore_restart_irq;
+	struct mtk_i2c_ac_timing ac_timing;
 	const struct mtk_i2c_compatible *dev_comp;
 };
 
@@ -289,11 +307,26 @@ static void mtk_i2c_init_hw(struct mtk_i2c *i2c)
 	if (i2c->dev_comp->dcm)
 		writew(I2C_DCM_DISABLE, i2c->base + OFFSET_DCM_EN);
 
-	if (i2c->dev_comp->timing_adjust)
-		writew((i2c->clk_src_div - 1), i2c->base + OFFSET_CLOCK_DIV);
-
 	writew(i2c->timing_reg, i2c->base + OFFSET_TIMING);
 	writew(i2c->high_speed_reg, i2c->base + OFFSET_HS);
+
+	writew(i2c->ac_timing.ext, i2c->base + OFFSET_EXT_CONF);
+
+	if (i2c->dev_comp->timing_adjust) {
+		writew(i2c->clk_src_div - 1, i2c->base + OFFSET_CLOCK_DIV);
+		writew(i2c->ac_timing.scl_mis_comp,
+				   i2c->base + OFFSET_SCL_MIS_COMP_POINT);
+		writew(i2c->ac_timing.sda_timing,
+				   i2c->base + OFFSET_SDA_TIMING);
+		writew(i2c->ac_timing.scl_hl_ratio,
+				   i2c->base + OFFSET_SCL_HIGH_LOW_RATIO);
+		writew(i2c->ac_timing.hs_scl_hl_ratio,
+				   i2c->base + OFFSET_HS_SCL_HIGH_LOW_RATIO);
+		writew(i2c->ac_timing.sta_stop,
+				   i2c->base + OFFSET_STA_STO_AC_TIMING);
+		writew(i2c->ac_timing.hs_sta_stop,
+				   i2c->base + OFFSET_HS_STA_STO_AC_TIMING);
+	}
 
 	/* If use i2c pin from PMIC mt6397 side, need set PATH_DIR first */
 	if (i2c->have_pmic)
@@ -465,12 +498,6 @@ static int mtk_i2c_do_transfer(struct mtk_i2c *i2c, struct i2c_msg *msgs,
 		control_reg |= I2C_CONTROL_DIR_CHANGE | I2C_CONTROL_RS;
 
 	writew(control_reg, i2c->base + OFFSET_CONTROL);
-
-	/* set start condition */
-	if (i2c->speed_hz <= 100000)
-		writew(I2C_ST_START_CON, i2c->base + OFFSET_EXT_CONF);
-	else
-		writew(I2C_FS_START_CON, i2c->base + OFFSET_EXT_CONF);
 
 	addr_reg = msgs->addr << 1;
 	if (i2c->op == I2C_MASTER_RD)
@@ -758,19 +785,36 @@ static const struct i2c_algorithm mtk_i2c_algorithm = {
 
 static int mtk_i2c_parse_dt(struct device_node *np, struct mtk_i2c *i2c)
 {
+	u16 ac_timing[7] = {0};
 	int ret;
 
 	ret = of_property_read_u32(np, "clock-frequency", &i2c->speed_hz);
 	if (ret < 0)
 		i2c->speed_hz = I2C_DEFAULT_SPEED;
 
-	if (!i2c->dev_comp->timing_adjust) {
-		ret = of_property_read_u32(np, "clock-div", &i2c->clk_src_div);
-		if (ret < 0)
-			return ret;
+	ret = of_property_read_u32(np, "clock-div", &i2c->clk_src_div);
+	if (ret < 0)
+		return ret;
 
-		if (i2c->clk_src_div == 0)
-			return -EINVAL;
+	if (i2c->clk_src_div == 0)
+		return -EINVAL;
+
+	ret = of_property_read_u16_array(np, "ac-timing", ac_timing,
+					 ARRAY_SIZE(ac_timing));
+
+	if (ret < 0) {
+		if (i2c->speed_hz <= I2C_DEFAULT_SPEED)
+			i2c->ac_timing.ext = I2C_ST_START_CON;
+		else
+			i2c->ac_timing.ext = I2C_FS_START_CON;
+	} else {
+		i2c->ac_timing.ext = ac_timing[0];
+		i2c->ac_timing.scl_hl_ratio = ac_timing[1];
+		i2c->ac_timing.hs_scl_hl_ratio = ac_timing[2];
+		i2c->ac_timing.scl_mis_comp = ac_timing[3];
+		i2c->ac_timing.sta_stop = ac_timing[4];
+		i2c->ac_timing.hs_sta_stop = ac_timing[5];
+		i2c->ac_timing.sda_timing = ac_timing[6];
 	}
 
 	i2c->have_pmic = of_property_read_bool(np, "mediatek,have-pmic");
@@ -826,9 +870,6 @@ static int mtk_i2c_probe(struct platform_device *pdev)
 	ret = mtk_i2c_parse_dt(pdev->dev.of_node, i2c);
 	if (ret)
 		return -EINVAL;
-
-	if (i2c->dev_comp->timing_adjust)
-		i2c->clk_src_div = I2C_DEFAULT_CLK_DIV;
 
 	if (i2c->have_pmic && !i2c->dev_comp->pmic_i2c)
 		return -EINVAL;
