62159e0833f8f6fc11bf205755a129bdaee2e9c8
jg_daolongzhu <jg_daolongzhu@mediatek.com>
2019-11-13 16:33:44 +0800

    [ALPS04882103] i2c: mt8167: move dma reset before i2c reset when hw_init
    [Detail]
    Our i2c driver default do dma reset after i2c reset
    in mtk_i2c_init_hw after kfree, but sometimes i2c reset
    trigger dma tx2rx, then dma write data to released dram.
    Move dma reset before i2c reset in mtk_i2c_init_hw to fix this bug.
    
    Test: build pass
    
    Change-Id: Icd131747450731862eb05d521b34ac2974743d66
    Signed-off-by: jg_daolongzhu <jg_daolongzhu@mediatek.com>
    CR-Id: ALPS04882103
    Feature: i2c

--- drivers/i2c/busses/i2c-mt65xx.c
+++ drivers/i2c/busses/i2c-mt65xx.c
@@ -296,6 +296,10 @@ static void mtk_i2c_init_hw(struct mtk_i2c *i2c)
 {
 	u16 control_reg;
 
+	writel(I2C_DMA_HARD_RST, i2c->pdmabase + OFFSET_RST);
+	udelay(50);
+	writel(I2C_DMA_CLR_FLAG, i2c->pdmabase + OFFSET_RST);
+
 	writew(I2C_SOFT_RST, i2c->base + OFFSET_SOFTRESET);
 
 	/* Set ioconfig */
@@ -336,10 +340,6 @@ static void mtk_i2c_init_hw(struct mtk_i2c *i2c)
 		      I2C_CONTROL_CLK_EXT_EN | I2C_CONTROL_DMA_EN;
 	writew(control_reg, i2c->base + OFFSET_CONTROL);
 	writew(I2C_DELAY_LEN, i2c->base + OFFSET_DELAY_LEN);
-
-	writel(I2C_DMA_HARD_RST, i2c->pdmabase + OFFSET_RST);
-	udelay(50);
-	writel(I2C_DMA_CLR_FLAG, i2c->pdmabase + OFFSET_RST);
 }
 
 /*
